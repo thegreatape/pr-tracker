0781c26ce28bc7291f5485967f952fed9c4ceeed
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2022-03-12 11:48:27 -0500

    more parsing

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,22 +4,39 @@ require 'exercise_set'
 class Parser
   # looks likes:
   # #Reverse ring fly
-  EXERCISE_NAME_RE = /\s*#(.*)\s*/
+  EXERCISE_NAME_RE = /
+      \s*            # optional whitespace
+      \#(?<name>.*)  # pound sign followed by name of exercise
+      \s*            # optional whitespace
+    /x
 
-  # TODO make these readable with extended re?
-  SETS_RE = /(\d+)\s*x\s*(\d+)\s*x\s*(\d+)/
+  SETS_RE = /
+    (?<weight>\d+) # weight
+    \s*x\s*        # x surrounded by optional whitespace
+    (?<reps>\d+)   # reps
+    (?:            # optionally followed by
+      \s*x\s*         # x surrounded by optional whitespace
+      (?<sets>\d+)    # sets
+    )?
+    /x
 
   def parse(contents)
     workout = Workout.new
     current_exercise = nil
+
     contents.split("\n").each do |line|
       if match = EXERCISE_NAME_RE.match(line)
-        puts "exercise"
-        current_exercise = Exercise.new(match[1])
+        current_exercise = Exercise.new(match[:name])
         workout.exercises << current_exercise
       elsif match = SETS_RE.match(line)
-        puts "sets"
-        current_exercise.sets << ExerciseSet.new(reps: match[2].to_i, weight_lbs: match[1].to_i)
+        set_count = (match[:sets] || 1).to_i
+        set_count.times do
+          current_exercise.sets << ExerciseSet.new(reps: match[:reps].to_i, weight_lbs: match[:weight].to_i)
+        end
+      else
+        if !line.chomp.empty?
+          puts "didn't match line:\n#{line}"
+        end
       end
     end
     workout
