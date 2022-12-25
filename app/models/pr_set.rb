58d57d0d2933a8efc78c0078eb6976c128d131e0
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2022-12-25 10:22:36 -0500

    formatting + don’t use 1rm formula for… 1rm

--- app/models/pr_set.rb
+++ app/models/pr_set.rb
@@ -2,6 +2,8 @@ class PrSet < ApplicationRecord
   belongs_to :exercise
 
   def e1rm
+    return weight_lbs.to_i if reps == 1
+
     if reps <= 10
       ((weight_lbs * 0.033 * reps) + weight_lbs).to_i
     end
