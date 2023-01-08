934ea5bd655294023eedc0c9e482070cbfaeffb0
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2023-01-08 08:56:31 -0500

    wip crud

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,21 @@ class WorkoutsController < ApplicationController
   def index
     @workouts = Workout.order(date: :desc).page(params[:page])
   end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    if @workout.update(params.require(:workout).permit(:date, :raw_text))
+      redirect_to workout_path(@workout), notice: "Workout updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
