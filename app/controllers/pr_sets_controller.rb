66b5cf09f1f5e24718b78530a1de7a10635c57e7
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2025-05-17 10:17:11 -0400

    See any exercise’s latest and all-time PRs (#6)

--- app/controllers/pr_sets_controller.rb
+++ app/controllers/pr_sets_controller.rb
@@ -3,23 +3,23 @@ class PrSetsController < ApplicationController
 
   def index
     @title = "PRs"
-    @pr_sets = pr_sets
+    @pr_sets = pr_sets(only_latest_pr: true)
   end
 
   def latest
     @title = "Latest PRs"
-    @pr_sets = pr_sets.order(date: :desc)
+    @pr_sets = pr_sets(only_latest_pr: false).order(date: :desc)
   end
 
   private
 
-  def pr_sets
-    exercises = Exercise.where(benchmark_lift: true)
+  def pr_sets(only_latest_pr:)
+    exercises = params[:exercise_id] ? Exercise.where(id: params[:exercise_id]) : Exercise.where(benchmark_lift: true)
     current_user
       .exercise_sets
       .pr_sets
       .joins(:exercise, :workout)
-      .where(exercise: exercises, latest_pr: true)
-      .or(current_user.exercise_sets.where(exercise: {synonym_of: exercises}, latest_pr: true))
+      .where(exercise: exercises, latest_pr: only_latest_pr)
+      .or(current_user.exercise_sets.where(exercise: {synonym_of: exercises}, latest_pr: only_latest_pr))
   end
 end
